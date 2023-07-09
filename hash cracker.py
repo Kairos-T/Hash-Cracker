9f6bc73ce7fe1fce025494d097acbe06e82ba1ab
Kairos Tay <80029462+Kairos-T@users.noreply.github.com>
2023-07-09 21:44:09 +0800

    Added salt value to password

--- hash cracker.py
+++ hash cracker.py
@@ -1,13 +1,21 @@
 import hashlib
+import bcrypt
+import os
 
 hash_to_crack = input("Enter the hash to crack: ").strip().lower()
+dictionary_path = input("Enter the path to the dictionary file: ")
 
-with open("dictionary.txt", "r") as f:
+if not os.path.exists(dictionary_path) or not os.path.isfile(dictionary_path):
+    print("Invalid dictionary file path.")
+    exit()
+
+with open(dictionary_path, "r") as f:
     for line in f:
-        password = line.strip().lower()
-        hash_value = hashlib.sha256(password.encode()).hexdigest()
+        password = line.strip().lower().encode()
+        salt = bcrypt.gensalt()
+        hash_value = bcrypt.hashpw(password, salt).decode()
         if hash_value == hash_to_crack:
-            print(f"Password found: {password}")
+            print(f"Password found: {password.decode()}")
             break
     else:
         print("Password not found in the dictionary")
